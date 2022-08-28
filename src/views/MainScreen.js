c8e1aca7d111ee81a935d3732e64593ac7d33072
lainshiHenry <henryle913@outlook.com>
2022-08-27 23:41:40 -0500

    initial commit

---
+++ src/views/MainScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './MainScreen.css'
+
+const MainScreen = () => {
+    return (
+        <div>
+            <header>
+                <ul >
+                    <li></li>
+                    <li></li>
+                    <li>Title</li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </header>
+            <section>
+
+            </section>
+        </div>
+    )
+}
+
+export default MainScreen
